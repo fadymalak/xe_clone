53e2b9808983ec1e13f456b5c603037ca29183b6
ahmadsameh8 <ahmadsameh7810@gmail.com>
2024-07-30 10:03:50 +0300

    model user and currency

---
+++ app/models.py/Currency.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Currency(models.Model):
+    """A model containing a currency with name and symbol."""
+
+    name = models.CharField(max_length=100)
+    symbol = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.name
