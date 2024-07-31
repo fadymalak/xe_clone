fd7112cf7d7a77ddc3a54a626f68a1381812469c
Shahd <shahd.kh@aucegypt.edu>
2024-08-01 01:06:01 +0300

    added urls

--- xe_clone/urls.py
+++ xe_clone/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from app.views import SignUpView, LoginView, TransactionHistoryView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('history/', TransactionHistoryView.as_view(), name='history'),
 ]
