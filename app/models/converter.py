daac0256e345ca9aeb8ed8f42a28d9d713d12f85
ahmadsameh8 <ahmadsameh7810@gmail.com>
2024-08-08 16:13:57 +0300

    serializers and tests

---
+++ app/models/converter.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class converter(models.Model):
+    """Model of currency converter.
+
+    Fields:
+        from_currency (Currency): The currency to convert from.
+        to_currency (Currency): The currency to convert to.
+        rate (Float): The conversion rate.
+        created_at (DateTime): The date when the converter was created.
+        updated_at (DateTime): The date when the converter was last updated.
+        deleted_at (DateTime): The date when the converter was deleted.
+    """
+
+    from_currency = models.ForeignKey("Currency", on_delete=models.CASCADE)
+    to_currency = models.ForeignKey("Currency", on_delete=models.CASCADE)
+    # TODO: Change to DecimalField or IntegerField
+    rate = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        """String representation of the converter."""
+        return f"{self.from_currency} to {self.to_currency}"
